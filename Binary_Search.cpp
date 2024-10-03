c59f1334d2b85b19c614feb6167b48a52a2c2786
Anshuman7080 <164294129+Anshuman7080@users.noreply.github.com>
2024-10-03 17:43:37 +0000

    First commit

--- Binary_Search.cpp
+++ Binary_Search.cpp
@@ -1,46 +1,57 @@
 
 #include <iostream>
+#include <algorithm> // For std::sort
 using namespace std;
-int lin(int arr[],int n,int key){
-    int low=0;
-    int high=n-1;
-    int num;
-    
-    while(low<=high)
-    {
-    num=(low+high)/2;
-        if(key==arr[num])
-         return num;
-       else if(key<arr[num]){
-            high=num-1;
-        }
-        //  if(key>num){
-        else{
-            low=num+1;
+
+int lin(int arr[], int n, int key) {
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (key == arr[mid]) {
+            return mid;
+        } else if (key < arr[mid]) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
         }
     }
-    return -1;
+    return -1; // Key not found
 }
 
-int main()
-{
-    int key,n,i;
-    int arr[n];
-    cout<<"enter range ";
-    cin>>n;
-    for(i=0;i<n;i++){
-        cout<<"eneter the "<<i+1<<" number";
-        cin>>arr[i];
-    }
-    //int num
-    cout<<"enter the key ";
-    //cin>>key;
-    int data=lin(arr,n,5);
-    if(data==-1)
-    cout<<"the number you entered is out of scope";
-    else
-    cout<<"the number is placed at "<<data <<"position";
-  //cout<<"here the num you wants "<<data;
+int main() {
+    int n;
 
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Array size must be positive." << endl;
+        return 1; // Exit if invalid size
+    }
+
+    int* arr = new int[n]; // Dynamically allocate array
+
+    for (int i = 0; i < n; i++) {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    cout << "Enter the key to search: ";
+    int key;
+    cin >> key;
+
+    // Sort the array before performing binary search
+    sort(arr, arr + n);
+
+    int index = lin(arr, n, key);
+    if (index == -1) {
+        cout << "The number you entered is not in the array." << endl;
+    } else {
+        cout << "The number is found at index: " << index << endl;
+    }
+
+    delete[] arr; // Free allocated memory
     return 0;
 }
