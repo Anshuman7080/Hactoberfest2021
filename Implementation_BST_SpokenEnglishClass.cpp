e963be8f9a76cb5e930ac8f70034b992ac977e17
Anshuman7080 <164294129+Anshuman7080@users.noreply.github.com>
2024-10-06 17:47:50 +0000

    Optimized version

--- Implementation_BST_SpokenEnglishClass.cpp
+++ Implementation_BST_SpokenEnglishClass.cpp
@@ -1,249 +1,164 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
 
 using namespace std;
 
-class Node{
-
-  public:
-
+class Node {
+public:
     int data;
-
     Node *left;
-
     Node *right;
 
-  
-
-  public:
-
-    Node(int x){
-
-      data = x;
-
-      left = NULL;
-
-      right = NULL;
-
-    }
-
+    Node(int x) : data(x), left(nullptr), right(nullptr) {}
 };
 
-Node *insert_BST(Node *tree, int val)
-
-{
-
-  if(tree == NULL){
-
-    cout<<val <<" is inserted into BST successfully"<<endl;
-
-    return (new Node(val));
-
-  }
-
-  if(tree->data >= val)
-
-    tree->left = insert_BST(tree->left, val);
-
-  else
-
-    tree->right = insert_BST(tree->right, val);
-
-  return tree;
-
-}
-
-bool searchInBST(Node *root , int k) {
-	// Write your code here
-    if(root==NULL){
-        return false;
+Node* insert_BST(Node* tree, int val) {
+    if (tree == nullptr) {
+        cout << val << " is inserted into BST successfully" << endl;
+        return new Node(val);
     }
-    if(root->data==k){
-        return true;
+    if (val <= tree->data) {
+        tree->left = insert_BST(tree->left, val);
+    } else {
+        tree->right = insert_BST(tree->right, val);
     }
-    else if(root->data>k){
-    	return searchInBST(root->left ,k);
-    }
-    else if(root->data<k){
-        return searchInBST(root->right ,k);
-    }
-    
-}
-Node* delete_BST(Node *tree, int val)
-
-{
-
-  if(tree == NULL){
-
-    cout<<"value is not present in BST"<<endl;
-
     return tree;
-
-  }
-
-  if(tree->data > val)
-
-    tree->left = delete_BST(tree->left, val);
-
-  else if(tree->data < val)
-
-    tree->right = delete_BST(tree->right, val);
-
-  else{
-
-    if(tree->left == NULL){
-
-      Node *temp = tree->right;
-
-      free(tree);
-
-      tree= temp;
-
-    }
-
-  
-    else if(tree->right == NULL){
-
-      Node *temp = tree->left;
-
-      tree = temp;
-
-      free(temp);
-
-    }
-
-  
-    else{
-
-      Node *temp = tree->left;
-
-      while(temp->right->right!=NULL){   
-
-        temp = temp->right;
-
-      }
-
-      tree->data = temp->right->data;       
-
-      Node *temp2 = temp->right->left;      
-
-      free(temp->right);                    
-
-      temp->right = temp2;                  
-
-    }
-
-  }
-
-  return tree;
-
 }
 
-void inorder(Node *tree)
+bool searchInBST(Node* root, int k) {
+    if (root == nullptr) return false;
+    if (root->data == k) return true;
+    return (k < root->data) ? searchInBST(root->left, k) : searchInBST(root->right, k);
+}
 
-{
+Node* findMin(Node* node) {
+    while (node->left != nullptr) {
+        node = node->left;
+    }
+    return node;
+}
 
-    if (tree != NULL)
+Node* delete_BST(Node* tree, int val) {
+    if (tree == nullptr) {
+        cout << "Value is not present in BST" << endl;
+        return tree;
+    }
 
-    {
+    if (val < tree->data) {
+        tree->left = delete_BST(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = delete_BST(tree->right, val);
+    } else {
+        // Node to be deleted found
+        if (tree->left == nullptr) {
+            Node* temp = tree->right;
+            delete tree;
+            return temp;
+        } else if (tree->right == nullptr) {
+            Node* temp = tree->left;
+            delete tree;
+            return temp;
+        }
 
+        // Node with two children: get the inorder successor (smallest in the right subtree)
+        Node* temp = findMin(tree->right);
+        tree->data = temp->data;  // Copy the inorder successor's value
+        tree->right = delete_BST(tree->right, temp->data);  // Delete the inorder successor
+    }
+    return tree;
+}
+
+void inorder(Node* tree) {
+    if (tree != nullptr) {
         inorder(tree->left);
-
-        cout<<tree->data<<" ";
-
+        cout << tree->data << " ";
         inorder(tree->right);
-
     }
-
 }
-void printLevelATNewLine(Node *root) {
-    if (root == NULL) {
-        return;
-    }
-    queue<Node *> q;
+
+void printLevelATNewLine(Node* root) {
+    if (root == nullptr) return;
+
+    queue<Node*> q;
     q.push(root);
-    q.push(NULL);
+    q.push(nullptr);  // Marker for end of level
+
     while (!q.empty()) {
-        Node *first = q.front();
+        Node* node = q.front();
         q.pop();
-        if (first == NULL) {
-            if (q.empty()) {
+        if (node == nullptr) {
+            cout << endl;  // End of level
+            if (!q.empty()) {
+                q.push(nullptr);  // Add marker for next level
+            }
+        } else {
+            cout << node->data << " ";
+            if (node->left != nullptr) {
+                q.push(node->left);
+            }
+            if (node->right != nullptr) {
+                q.push(node->right);
+            }
+        }
+    }
+}
+
+int main() {
+    cout << "BRITISH COUNCIL CLASS" << endl;
+    Node* root = nullptr;
+    int z = 0;
+
+    while (z != 6) {
+        cout << "*****************" << endl;
+        cout << "Enter 1: To Insert student" << endl;
+        cout << "Enter 2: To Search for any student" << endl;
+        cout << "Enter 3: if any student exits" << endl;
+        cout << "Enter 4: To print the tree in order" << endl;
+        cout << "Enter 5: To print level wise" << endl;
+        cout << "Enter 6: To Exit" << endl;
+        cout << "*****************" << endl;
+
+        cin >> z;
+        switch (z) {
+            case 1: {
+                cout << "Enter the student roll no. as they enter the class: ";
+                int id;
+                cin >> id;
+                root = insert_BST(root, id);
                 break;
             }
-            cout << endl;
-            q.push(NULL);
-            continue;
-        }
-        cout << first->data << " ";
-        if (first->left != NULL) {
-            q.push(first->left);
-        }
-        if (first->right != NULL) {
-            q.push(first->right);
+            case 2: {
+                cout << "Enter the student Roll No. for search: ";
+                int k;
+                cin >> k;
+                cout << (searchInBST(root, k) ? "Student is PRESENT in the class" : "Student is NOT PRESENT in the class") << endl;
+                break;
+            }
+            case 3: {
+                cout << "Enter the student's Roll No. who exited: ";
+                int m;
+                cin >> m;
+                root = delete_BST(root, m);
+                break;
+            }
+            case 4:
+                inorder(root);
+                cout << endl;
+                break;
+            case 5:
+                printLevelATNewLine(root);
+                break;
+            case 6:
+                break;
+            default:
+                cout << "Invalid option. Please try again." << endl;
+                break;
         }
     }
-}
+    cout << "*******EXIT********" << endl;
 
-int main(){
-    cout<<"BRITISH COUNCIL CLASS"<<endl;
-    // cout<<"Root of the BST i.e. the first child ";
-    // int x;
-    // cin>>x;
-    // cout<<x<<endl;
-    Node *root = NULL;
-    //takeInput(root);
+    // Cleanup memory (not implemented, but good practice)
+    // Ideally, we should implement a function to delete the entire BST.
     
-    int z;
-    while(z!=6){
-    cout<<"*****************"<<endl;
-    cout<<"Enter 1: To Insert student"<<endl;
-    cout<<"Enter 2: To Search for any student"<<endl;
-    cout<<"Enter 3: if any student exits"<<endl;
-    cout<<"Enter 4: To print the tree in order"<<endl;
-    cout<<"Enter 5: To print level wise"<<endl;
-    cout<<"Enter 6: To Exit"<<endl;
-    cout<<"*****************"<<endl;
-    
-    cin >> z;
-    switch(z){
-        case 1:
-        cout<<"Enter the student roll no. as they enter the class "<<endl;
-        int id;
-        cin>>id;
-        root=insert_BST(root, id);
-        inorder(root);
-        break;
-        case 2:
-        int k;
-        cout<<"Enter the student Roll No. for search "<<endl;
-        cin>>k;
-        if(searchInBST(root,k)==false){
-            cout<<"Student is NOT PRESENT in the class"<<endl;
-        }
-        else{
-            cout<<"Student is PRESENT in the class"<<endl;
-        }
-        break;
-        case 3:
-        int m;
-        cout<<"Enter the student's Roll No. who exited "<<endl;
-        cin>>m;
-        delete_BST(root, m);
-        //printLevelATNewLine(root);
-        break;
-        case 4:
-        inorder(root);
-        break;
-        case 5:
-        printLevelATNewLine(root);
-        break;
-        case 6:
-        z=6;
-        break;
-    }
-    
-    }
-    // cout << ((searchInBST(root, k)) ? "true" : "false");
-    cout<<"*******EXIT********"<<endl;
-    delete root;
-
+    return 0;
 }
